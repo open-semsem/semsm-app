3b23e1f7a757ca26300693c2064f5ca00c929778
emanherawy <eman.herawy@gmail.com>
2020-09-10 17:20:33 +0200

    update team

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -19,93 +19,74 @@ export default function Team() {
       <Container>
         <h2 className="title">Our Team</h2>
         <Row>
-          <Col md="6">
+          <Col md="12">
             <Card className="card-profile card-plain">
-              <div className="card-avatar">
+              <div className="card-image">
                 <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                  {/* <img
-                        alt="..."
-                        src={require("assets/img/faces/clem-onojeghuo-3.jpg")}
-                      /> */}
+                  <img
+                    alt="..."
+                    src={require("assets/pic/EmanInHongKong.jpg")}
+                  />
                 </a>
               </div>
               <CardBody>
                 <a href="#pablo" onClick={(e) => e.preventDefault()}>
                   <div className="author">
                     <CardTitle tag="h4">Eman Herawy</CardTitle>
-                    {/* <h6 className="card-category">Product Manager</h6> */}
+                    <h6 className="card-category">Founder</h6>
                   </div>
                 </a>
                 <p className="card-description text-center">
                   Blockchain full stack developer .
-                  Mentor & Reviewer @Udacity
-                  Devcon 5 scholar alumni @ethereum foundation,Truffle University alumni, MSc in Cloud computing networks.
+                  Mentor & Reviewer @Udacity,
+                  Devcon 5 scholar alumni @ethereum foundation,Truffle University alumni & MSc in Cloud computing networks.
                     </p>
               </CardBody>
               <CardFooter className="text-center">
-                <Button
-                  className="btn-just-icon btn-neutral"
-                  color="link"
-                  href="https://twitter.com/@eman_herawy"
-                  // onClick={(e) => e.preventDefault()}
-                >
-                  <i className="fa fa-twitter" />
-                </Button>
-              
                 <Button
                   className="btn-just-icon btn-neutral ml-1"
                   color="link"
                   href="https://www.linkedin.com/in/emanherawy"
-                  // onClick={(e) => e.preventDefault()}
+                // onClick={(e) => e.preventDefault()}
                 >
                   <i className="fa fa-linkedin" />
                 </Button>
-              </CardFooter>
-            </Card>
-          </Col>
-          <Col md="6">
-            <Card className="card-profile card-plain">
-              <div className="card-avatar">
-                <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                  {/* <img
-                        alt="..."
-                        src={require("assets/img/faces/joe-gardner-2.jpg")}
-                      /> */}
-                </a>
-              </div>
-              <CardBody>
-                <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                  <div className="author">
-                    <CardTitle tag="h4">Maged M. El Jazzar</CardTitle>
-                    {/* <h6 className="card-category">Designer</h6> */}
-                  </div>
-                </a>
-                <p className="card-description text-center">
-                Former CTO Tradeflo - IBM incubated startup in 2019, and teach lead in Deep Cloud AI - AI driven decentralized cloud computing system. Has several publications and MSc degree in AI and Blockchain.
-                    </p>
-              </CardBody>
-              <CardFooter className="text-center">
                 <Button
                   className="btn-just-icon btn-neutral"
                   color="link"
-                  href="https://twitter.com/@mmeljazzar"
-                  // onClick={(e) => e.preventDefault()}
+                  href="https://twitter.com/@eman_herawy"
+                // onClick={(e) => e.preventDefault()}
                 >
                   <i className="fa fa-twitter" />
                 </Button>
-             
                 <Button
-                  className="btn-just-icon btn-neutral ml-1"
+                  className="btn-just-icon btn-neutral"
                   color="link"
-                  href="https://www.linkedin.com/in/mmjazzar/ "
-                  // onClick={(e) => e.preventDefault()}
+                  href="https://medium.com/@emanherawy"
+                // onClick={(e) => e.preventDefault()}
                 >
-                  <i className="fa fa-linkedin" />
+                  <i className="fa fa-medium" />
                 </Button>
+                <Button
+                  className="btn-just-icon btn-neutral"
+                  color="link"
+                  href="https://www.upwork.com/freelancers/~01edc5405b9c4be8a2"
+                // onClick={(e) => e.preventDefault()}
+                >
+                  <i className="fa" >
+                    <img
+                      alt="Operation"
+                      src={require('../../assets/pic/upwork-icon.svg')}
+
+                    />
+                  </i>
+                </Button>
+
+
               </CardFooter>
             </Card>
           </Col>
-         
+
         </Row>
       </Container>
     </div>
