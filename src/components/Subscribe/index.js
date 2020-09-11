02fb46432b06c5081c8d3f7c7bb98d16a4a47338
emanherawy <eman.herawy@gmail.com>
2020-09-11 03:31:46 +0200

    fix miner issue

--- src/components/Subscribe/index.js
+++ src/components/Subscribe/index.js
@@ -17,7 +17,7 @@ export default function WorkCycle() {
               <Col className="ml-auto mr-auto" md="12">
                 <h2 className="text-center">Subscribe?</h2>
        <div>
-       <iframe src="https://semsm.substack.com/embed" width="100%" height="320" style={{border:'1px solid #EEE', background:'white'}} frameborder="0" scrolling="no"></iframe>
+       <iframe src="https://semsm.substack.com/embed" width="100%" height="320" style={{border:'1px solid #EEE', background:'white'}} frameBorder="0" scrolling="no"></iframe>
             
        </div>
          </Col>
